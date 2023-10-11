5df5f519ab7d1ebb9495e9607de68c2aab918c55
TED.F <ted.fang@sjfood.com>
2023-10-11 16:35:29 +0800

    Update ThisAddIn.cs

--- UpdateSetting/UpdateSetting/ThisAddIn.cs
+++ UpdateSetting/UpdateSetting/ThisAddIn.cs
@@ -6,7 +6,7 @@ namespace UpdateSetting
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            MessageBox.Show("Hello nice to meet you");
+            MessageBox.Show("Hello nice to meet you!");
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
